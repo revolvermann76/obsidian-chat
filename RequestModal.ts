ec6fec009b6a4e3cf114ea5a30539f265ffadee0
Marc Langer <postkutsche@gmail.com>
2023-12-28 17:35:43 +0100

    ...

--- RequestModal.ts
+++ RequestModal.ts
@@ -1,9 +1,11 @@
-import { Chat } from "Chat";
+import { Chat, TUtterance } from "Chat";
 import { MultiSuggest } from "MultiSuggest";
 import { Modal, App, ButtonComponent } from "obsidian"
 export class RequestModal extends Modal {
 
     #chat: Chat;
+    #resultCnt: HTMLDivElement;
+    #promptInput: HTMLInputElement;
 
     constructor(app: App, apiKey: string) {
         super(app);
@@ -17,31 +19,78 @@ export class RequestModal extends Modal {
 
 
         // Prompt input
-        const promptInput = document.createElement("input");
-        promptInput.addClass("prompt-input");
-        promptInput.setAttribute("placeholder", "Type your request here ...")
-        const ms = new MultiSuggest(promptInput, this.onRequest.bind(this), app);
+        this.#promptInput = document.createElement("input");
+        this.#promptInput.addClass("prompt-input");
+        this.#promptInput.setAttribute("placeholder", "Type your request here ...")
+        const ms = new MultiSuggest(this.#promptInput, this.onRequest.bind(this), app);
         ms.setContent(new Set(["Lirum", "Larum", "Bla blubbb"]))
-        this.contentEl.appendChild(promptInput);
+        this.contentEl.appendChild(this.#promptInput);
 
         // Result
-        const resultCnt = document.createElement("div");
-        resultCnt.addClass("chat-result-container");
-        this.contentEl.appendChild(resultCnt)
+        this.#resultCnt = document.createElement("div");
+        this.#resultCnt.addClass("chat-result-container");
+        this.contentEl.appendChild(this.#resultCnt)
 
         // Buttons
         const buttonCnt = document.createElement("div");
         buttonCnt.addClass("chat-button-container");
         this.contentEl.appendChild(buttonCnt);
 
-        new ButtonComponent(buttonCnt).setButtonText("Stop").setClass("chat-stop-button")
-        new ButtonComponent(buttonCnt).setButtonText("Copy").setClass("chat-copy-button")
-        new ButtonComponent(buttonCnt).setButtonText("Copy All").setClass("chat-copy-all-button")
+        new ButtonComponent(buttonCnt).setButtonText("Stop").setClass("chat-stop-button").onClick(() => {
+            this.#chat.abort();
+            this.#promptInput.focus();
+        })
+
+        new ButtonComponent(buttonCnt).setButtonText("Clear Conversation").setClass("chat-clear-button").onClick(() => {
+            this.#chat.reset();
+            this.#resultCnt.empty();
+            this.#promptInput.focus();
+        })
+
+        new ButtonComponent(buttonCnt).setButtonText("Copy Response").setClass("chat-copy-button").onClick(() => {
+            console.log(this.#chat.conversation.pop()?.content);
+            this.close();
+        })
+        new ButtonComponent(buttonCnt).setButtonText("Copy Conversation").setClass("chat-copy-all-button").onClick(() => {
+            let lines: string[] = [];
+            const c = this.#chat.conversation;
+            for (let i = 0; i < c.length; i++) {
+                const u: TUtterance = c[i];
+                console.log(u);
+                if (u.role === "assistant") {
+                    lines.push(u.content);
+                } else if (u.role === "user") {
+                    lines.push(`**${u.content}**`);
+                }
+            }
+            console.log(lines.join("\n"));
+            this.close();
+        })
 
     }
 
+    #scrollDown() {
+        this.#resultCnt.scrollTo(0, this.#resultCnt.scrollHeight);
+    }
+
     onRequest(prompt: string) {
-        alert(prompt)
+        const h = document.createElement("div");
+        h.addClass("cm-strong");
+        h.innerHTML = prompt;
+        this.#resultCnt.appendChild(h);
+        const div = document.createElement("div");
+        div.addClass("response");
+        this.#resultCnt.appendChild(div);
+        this.#chat.stream(prompt, {
+            partHandler: (part) => {
+                div.innerHTML = part;
+                this.#scrollDown();
+            }
+        }).then((result) => {
+            div.innerHTML = result;
+            this.#scrollDown();
+            this.#promptInput.focus();
+        })
     }
 
     onOpen() {
